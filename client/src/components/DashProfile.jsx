f099d441082a200cd3d36623059348a294fd7cc2
balaj <balaj2203e@aptechgn.net>
2024-02-22 11:50:00 -0500

    Complete sidebar of dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Sidebar} from 'flowbite-react'
+import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+export default function DashProfile() {
+  return (
+   <div className="">das</div>
+  )
+}
--- client/src/pages/Dasboard.jsx
+++ client/src/pages/Dasboard.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-export default function Dasboard() {
-  return (
-    <div>
-      <h1 className=''>Dashboard</h1>
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 
+export default function Dasboard() {
+  const locaction = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(locaction.search)
+    const tabFromUrl = urlParams.get('tab')
+      if (tabFromUrl){
+        setTab(tabFromUrl);
+      }
+  },[location.search])
+  
+  return( 
+  <div className="min-h-screen flex flex-col md:flex-row">
+      <div className='md:w-56'>
+        {/* Sidebar*/}
+        <DashSidebar/>
+      </div>
+      {/* profile */}
+{tab === 'profile' && <DashProfile/>}
     </div>
   )
 }
